83d2f3f8949378f4fbfe04ba281cdab8d54fef7d
Felicia Salim <feliciasalim08@gmail.com>
2025-11-16 22:57:13 +0700

    fix

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -11,14 +11,17 @@ X_test,  y_test  = df_test.drop('stroke', axis=1),  df_test['stroke']
 
 mlflow.set_experiment("stroke-prediction")
 
-mlflow.sklearn.autolog(log_models=True, registered_model_name="stroke-prediction-model")
-
 model = RandomForestClassifier(
     class_weight='balanced',
     random_state=42
 )
 
-model.fit(X_train, y_train)
-accuracy = model.score(X_test, y_test)
-print(f"Test Accuracy: {accuracy}")
-print("Model logged automatically by autolog!")
+with mlflow.start_run(run_name="stroke-prediction"):
+    mlflow,autolog()
+    model.fit(X_train, y_train)
+    accuracy = model.score(X_test, y_test)
+    
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.sklearn.log_model(model, "model")
+    
+    print(f"Test Accuracy: {accuracy}")
